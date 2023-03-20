f1f2abbd460607f9581ae7dca88b0b7b913fc520
Cris Montajes <cristajesawa@gmail.com>
2023-03-20 08:53:58 +0800

    Frontend

---
+++ website/auth.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@auth.route("/logout")
+def logout():
+    return "<p>logout</p>"
+
+
+@auth.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
